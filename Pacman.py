7669334390e3ac982457f7de7fbe31198f438e82
luis.tejaxun <luistejaxunrz@gmail.com>
2023-03-18 08:17:21 -0600

    18.03.2023 - Initial pacman paths with exception in (3,3)

---
+++ Pacman.py
@@ -0,0 +1,90 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+class Grid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+
+    def in_bounds(self, id):
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, id):
+        return id not in self.walls
+
+    def neighbors(self, id):
+        (x, y) = id
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+    def cost(self, current, next):
+        return 1
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
+
+# Example usage
+grid = Grid(5, 5)
+grid.walls = [(1, 1), (2, 1), (3, 1), (1, 3), (2, 3), (3, 3)]
+start = (0, 0)
+goals = [(4, 4), (4, 3), (3, 4), (3, 3)]
+paths = []
+
+for goal in goals:
+    came_from, cost_so_far = a_star_search(grid, start, goal)
+    path = []
+    current = goal
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start)
+    path.reverse()
+    paths.append(path)
+
+# Find the shortest path
+shortest_path = min(paths, key=len)
+
+print(f"All paths: {paths}")
+print(f"Shortest path: {shortest_path}")
